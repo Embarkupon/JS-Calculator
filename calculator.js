522abbf1bb3e31498c6b7c573d345a4797212ced
Embarkupon <165702416+Embarkupon@users.noreply.github.com>
2024-11-25 11:36:47 -0500

    the calculator displays text values from pressing the buttons

--- calculator.js
+++ calculator.js
@@ -1,31 +1,44 @@
 let operands = new Object();
+let displayText = document.querySelector(".display-text");
+let numberStream = [];
 
-function operation(obj) {
+let keypad = document.querySelectorAll(".key");
+keypad.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        numberStream.push(btn.innerHTML);
+        let value_x = Number(numberStream.join(""));
+        displayText.textContent = value_x;
+        console.log(value_x + typeof value_x);
+    });
+}) 
+
+
+function operation(obj, text) {
     obj.operand_x = 3;
     obj.operand_y = 5;
     obj.operator = "add";
     console.log(obj);
     switch (obj.operator) {
         case "add":
-            add(obj.operand_x, obj.operand_y);
+            add(obj.operand_x, obj.operand_y, text);
             break;
         default:
             console.log("No operation was performed.");
         }
 }
 
-function add(x,y) {
-    console.log(x + y);
+function add(x,y,display) {
+    display.textContent = x + y;
 }
 
-function subtract(x,y) {
+function subtract(x,y,display) {
     
 }
 
-function multiply(x,y) {
+function multiply(x,y,display) {
     
 }
 
-function divide(x,y) {
+function divide(x,y,display) {
     
 }
