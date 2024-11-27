cf90c5c95eb94166f8e1cb041398b79cd697f528
Embarkupon <165702416+Embarkupon@users.noreply.github.com>
2024-11-27 15:20:04 -0500

    user can enter decimals and negative numbers

--- calculator.js
+++ calculator.js
@@ -1,28 +1,53 @@
-let operands = {
-    operand_x: 0,
-};
+let numberStream = [];
+let operands = {operand_x: 0,};
 let displayText = document.querySelector(".display-text");
 let displayOperator = document.querySelector(".display-operator");
-displayText.textContent = operands.operand_x;
-let numberStream = [];
+displayText.textContent = 0;
 let pressed = false;
 
 let keypad = document.querySelectorAll(".key");
-//TODO: Evaluate for floats. numberstream should be limited to
-//A SINGLE decimal point after the decimal button is pressed
-//handle overflow. Implement proportion, negate, clear, delete.
+//TODO: handle overflow. Implement proportion, negate, clear, delete.
 keypad.forEach((btn) => {
     btn.addEventListener("click", () => {
-        if (btn.classList.contains("number")) {
-            numberStream.push(btn.innerHTML);
-            displayText.textContent = numberStream.join("");
+        if (btn.classList.contains("number")) {//everytime number is pressed
+            if (numberStream.length === 0) { 
+                numberStream.push(0); //calculator starts with 0
+            }
+            numberStream.push(btn.innerHTML); //store number pressed
+            if (numberStream[0] == 0 && !numberStream.includes(".")) {
+                numberStream.shift();//prevents leading 0s
+            }
+            displayText.textContent = numberStream.join("");//displays number
             if(pressed == true) {
                 operands.operand_y = +numberStream.join("");
             }
+        }
+        else if (btn.id == "decimal") {
+            if (numberStream.length === 0) {
+                numberStream.push(0);
+            }
+            if (!numberStream.includes(btn.innerHTML)) {
+                numberStream.push(btn.innerHTML);
+                displayText.textContent = numberStream.join("");
+            } 
+        }
+        else if (btn.id == "negate") {
+            if (numberStream.length !== 0) {
+                if (!numberStream.includes("-")) {
+                    if (!+numberStream.join("")) {
+                        
+                    } else {
+                        numberStream.unshift("-")
+                    }
+                    if(pressed == true) {
+                        operands.operand_y = +numberStream.join("");
+                    }
+                    displayText.textContent = numberStream.join("");
+                }
+            }
         } 
-        
         else if (btn.classList.contains("operation")) {
-            console.log(pressed);
+            console.log("flag in, " + pressed);
             if (pressed == false && btn.id !== "evaluate") {
                 pressed = true;
                 if (numberStream.length !== 0) {
@@ -31,50 +56,36 @@ keypad.forEach((btn) => {
             } 
             else if (pressed == true && btn.id !== "evaluate") {
                 if (numberStream.length !== 0) {
-                    operands.evaluation = operation(operands);
+                    operation(operands);
                     displayText.textContent = operands.evaluation;
-                    operands.operand_x = operands.evaluation;
                 }
+                
             } 
             else if (btn.id === "evaluate") {
+                pressed = false;
                 if ("operand_y" in operands) {
-                    pressed = false;
-                    operands.evaluation = operation(operands);
+                    operation(operands);
                     displayText.textContent = operands.evaluation;
-                    operands.operand_x = operands.evaluation;
-                    delete operands.operand_y;
                 } else if (numberStream.length !== 0) {
                     operands.operand_x = +numberStream.join("");
-                }
+                    operation(operands);
+                    displayText.textContent = operands.evaluation;
+                } 
             }
             numberStream = [];
             operands.operator = btn.id;
             displayOperator.textContent = btn.innerHTML;
 
-            console.log(pressed);
-            console.log(operands.operator);
-            console.log(operands.operand_x);
-            console.log(operands.operand_y);
-
-            /*let functionKeys = document.querySelectorAll(".operation");
-            functionKeys.forEach((key) => {
-                key.disabled = true;
-            });*/
+            console.log("flag out, "+pressed);
+            console.log("operator, "+operands.operator);
+            console.log("x, "+operands.operand_x);
+            console.log("y, "+operands.operand_y);
         } 
-
-        else {
-            console.log(btn.classList);
-        }
     });
 }) 
 
-
 function operation(obj) {
-    //obj.operand_x = 3;
-    //obj.operand_y = 5;
-    //obj.operator = "add";
     let evaluation = 0;
-    console.log(obj);
     switch (obj.operator) {
         case "add":
             evaluation = add(obj.operand_x, obj.operand_y);
@@ -89,26 +100,15 @@ function operation(obj) {
             evaluation = divide(obj.operand_x, obj.operand_y);
             break;
         default:
+            evaluation = obj.operand_x;
             console.log("No operation was performed.");
         }
-    return evaluation;
+    operands.evaluation = evaluation;
+    operands.operand_x = operands.evaluation;
+    delete operands.operand_y;
 }
 
-function add(x,y) {
-    return x + y;
-}
-
-function subtract(x,y) {
-    return x - y;
-}
-
-function multiply(x,y) {
-    return x * y;
-}
-
-function divide(x,y) {
-    if (!y) {
-        return "bruh";
-    }
-    return x / y;
-}
+function add(x,y) {return x + y;}
+function subtract(x,y) {return x - y;}
+function multiply(x,y) {return x * y;}
+function divide(x,y) {return !y ? "bruh" : x / y;}
